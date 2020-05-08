a472f7b221ca54245b7b927a6fa112584069c91c
trophygeek <trophygeek@gmail.com>
2020-05-08 15:39:44 -0700

    - Modernized javascript to be es6 (var -> let/const etc) - Fixed video resize to use css instead of javascript on resize. - Added an easy way to play with youtube iframe parameters - Generated page background black  - Explicitly define youtube domains in manifest (better security AND chrome does domain matching for extension).  - Misc cleanup

--- JustVideoExtension/ksv_background.js
+++ JustVideoExtension/ksv_background.js
@@ -3,54 +3,78 @@
 // Licensed as free open-source in the Public Domain
 
 // inspired by safeyoutube.net and Wessam El Mahdy
-
+// chrome.webRequest.onBeforeRequest.addListener(function (details) {
+//       if (!isAllowed(details)) {
+//         trace('blocking chrome.webRequest.onBeforeRequest.addListener', details);
+//         return {cancel: true};    // fail
+//       }
+//     },
+//     {
+//       urls: ['https://*.imgur.com/*'],
+//       types: ['main_frame', 'sub_frame', 'stylesheet', 'script', 'image', 'xmlhttprequest', 'media']
+//     },
+//     ['blocking']);      // NOTE: NOT getting request headers, the extension can only see urls and block them.
 
 chrome.tabs.onUpdated.addListener( function( tabId,  changeInfo,  tab) {
-   var URL = tab.url;
-   if (tab.pendingUrl) {
-       console.log("kidsafeyoutube pending: " + tab.url);
-       URL = tab.pendingUrl;
-   } else {
-      console.log("kidsafeyoutube actual: " + tab.url);
-   }
-   
+  if (tabId.tabId === -1) { // chrome empty tab
+    return true;
+  }
 
-   var re = new RegExp("^http?s://[^/]*youtube.com/watch.*[?&]v=([^&]*)$")
-   var match = re.exec(URL);
-   if (match) {
-      var yt_id = match[1];
-      console.log("match! " + URL + " yt_id is " + yt_id);
+  // we use the title to detect if this navigation is happening in an iframe on our generated page.
+  // it's actually hard to detect if a change is an iframe page nav or a top level tab nav without using
+  // onBeforeRequest() ... which requires more permissions.
+  if (tab.title === 'Just Kids Youtube') {
+    return;
+  }
 
-      // generate a random URL...
-      var internalRedirectPage = chrome.runtime.getURL("ksv_player.html?yt_id=" + yt_id + "&nonce=" + Math.random());
-      console.log(internalRedirectPage);
+  const is_top_level_nav = (tab.openerTabId===tab.id);
 
-      // replace the watch page with internal landing page
-      chrome.tabs.update(tab.id, {url: internalRedirectPage});
-      chrome.history.deleteUrl({url: URL});
+  let url = tab.url;
+  if (tab.pendingUrl) {
+    console.log(`kidsafeyoutube PENDING. Is top level ${is_top_level_nav}` , changeInfo, tab);
+    url = tab.pendingUrl;
+  } else {
+    console.log(`kidsafeyoutube actual. Is top level ${is_top_level_nav}`, changeInfo, tab);
+  }
 
-      // get a safeyoutube link
-      // https://stackoverflow.com/questions/27091602/chrome-extension-communicate-with-rest-web-service
-      var x = new XMLHttpRequest();
-      x.open("GET", "https://safeYouTube.net/api/generate?yt_id=" + yt_id);
-      x.onload = function () {
-           console.log(x.responseText);
+  // the extension manifest should only allow youtube requests, so we can assume a bunch of stuff
+  const urlparse = new URL(url);
+  if (urlparse.protocol !== 'https:') {
+    debugger;
+  }
+  const yt_id = urlparse.searchParams.get('v');
+  if (yt_id) {
+    console.log(`match! ${url} yt_id: '${yt_id}'`);
 
-      };
-      x.send();
+    // generate a random URL...
+    const internalRedirectPage = chrome.runtime.getURL(`ksv_player.html?yt_id=${yt_id}&nonce=${Math.random()}`);
+    console.log(internalRedirectPage);
 
-   }
+    // remove this page from history
+    chrome.history.deleteUrl({url});
+    // replace the watch page with internal landing page
+    chrome.tabs.update(tab.id, {url: internalRedirectPage});
+
+
+    // // get a safeyoutube link
+    // // https://stackoverflow.com/questions/27091602/chrome-extension-communicate-with-rest-web-service
+    // var x = new XMLHttpRequest();
+    // x.open("GET", "https://safeYouTube.net/api/generate?yt_id=" + yt_id);
+    // x.onload = function () {
+    //      console.log(x.responseText);
+    //
+    // };
+    // x.send();
+
+  }
 });
-
-
-
 
 
 
 chrome.extension.onConnect.addListener(function(port) {
   port.onMessage.addListener(function(msg) {
-      // do nothing...
-    });
+    // do nothing...
+  });
 });
 
 chrome.extension.onRequest.addListener(function(request,sender,sendResponse) {
@@ -94,8 +118,8 @@ chrome.contextMenus.create(
       "onclick" : genericOnClick });
 
 */
-      
-    
+
+
 
 // NOTES solving problem with context menu not appearing in incognito
 //
